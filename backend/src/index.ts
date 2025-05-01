2f26f6ad2d1c0bd838e04a10710634d7828fa487
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-05-01 14:02:06 +0530

    First commit

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+require('dotenv').config();
+import Anthropic from "@anthropic-ai/sdk";
+
+const anthropic = new Anthropic();
+
+async function main() {
+  const msg = await anthropic.messages.create({
+    model: "claude-3-7-sonnet-20250219",
+    max_tokens: 1024,
+    messages: [{ role: "user", content: "Hello, Claude" }],
+  });
+  console.log(msg);
+}
+main();
